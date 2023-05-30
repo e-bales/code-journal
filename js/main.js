5ef5182ee4351fb679fdafa219536ea21c608cb6
Eli Bales <elibales21@gmail.com>
2023-05-30 16:49:09 +0000

    added renderEntry func and DOMContenentLoaded event listener

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const $photoURL = document.querySelector('#url');
 const $form = document.querySelector('form');
 const $image = document.querySelector('#img-preview');
+const $unorderedList = document.querySelector('ul');
 
 $photoURL.addEventListener('input', event => {
   const url = $photoURL.value;
@@ -23,4 +24,44 @@ $form.addEventListener('submit', event => {
   data.entries.unshift(obj);
   $image.setAttribute('src', 'images/placeholder-image-square.jpg');
   $form.reset();
+});
+
+function renderEntry(entry) {
+  const title = entry.title;
+  const notes = entry.notes;
+  const url = entry.url;
+  const $li = document.createElement('li');
+  const $rowDiv = document.createElement('div').setAttribute('class', 'row');
+  $li.appendChild($rowDiv);
+
+  // first column
+  const $imgCol = document.createElement('div').setAttribute('class', 'column-half');
+  $rowDiv.appendChild($imgCol);
+
+  const $imgWrap = document.createElement('div').setAttribute('class', 'img-wrapper');
+  $imgCol.appendChild($imgWrap);
+
+  const $img = document.createElement('img').setAttribute('src', url);
+  $imgCol.appendChild($img);
+
+  // second column
+  const $infoCol = document.createElement('div').setAttribute('class', 'column-half');
+  $rowDiv.appendChild($infoCol);
+
+  const $title = document.createElement('h2').setAttribute('class', 'entry-title');
+  $title.textContent = title;
+  $infoCol.appendChild($title);
+
+  const $notes = document.createElement('p');
+  $notes.textContent = notes;
+  $infoCol.appendChild($notes);
+
+  return $li;
+}
+
+document.addEventListener('DOMContentLoaded', event => {
+  for (let i = 0; i < data.entries.length; i++) {
+    const currEntry = data.entries[i];
+    $unorderedList.appendChild(renderEntry(currEntry));
+  }
 });
